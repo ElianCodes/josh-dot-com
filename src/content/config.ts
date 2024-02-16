5f35ac9c408e08d736ca97bf5dc79b7c04c08970
Elian Van Cutsem <hello@elian.codes>
2024-02-16 10:55:46 +0300

    wip: do some small refactor

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,33 @@
 import { rssSchema } from "@astrojs/rss";
 import { defineCollection, z } from "astro:content";
 
+const speakingCollection = defineCollection({
+	schema: z.object({
+		category: z.string(),
+		date: z.date(),
+		event: z.string(),
+		href: z.string(),
+		links: z.record(z.string()).optional(),
+		location: z.string().optional(),
+		title: z.string().optional(),
+	}),
+});
+
+const blogCollection = defineCollection({
+	schema: ({ image }) => rssSchema.extend({
+		description: z.string(),
+		download: z.string().optional(),
+		image: z
+			.object({
+				alt: z.string(),
+				src: image().or(z.string()),
+			})
+			// TODO: add images for older blog posts, then remove this .optional()
+			.optional(),
+	}),
+});
+
 export const collections = {
-	blog: defineCollection({
-		schema: rssSchema.extend({
-			description: z.string(),
-			download: z.string().optional(),
-			image: z
-				.object({
-					alt: z.string(),
-					src: z.string(),
-				})
-				// TODO: add images for older blog posts, then remove this .optional()
-				.optional(),
-		}),
-	}),
-	speaking: defineCollection({
-		schema: z.object({
-			category: z.string(),
-			date: z.date(),
-			event: z.string(),
-			href: z.string(),
-			links: z.record(z.string()).optional(),
-			location: z.string().optional(),
-			title: z.string().optional(),
-		}),
-	}),
+	blog: blogCollection,
+	speaking: speakingCollection,
 };
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,21 +1,21 @@
 import type { APIContext } from "astro";
-
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
 
 import { blogDescription, site } from "../constants";
 
 export async function GET(context: APIContext) {
+	const blogs = await getCollection("blog");
+
 	return rss({
-		customData: `<language>en-us</language>`,
+		customData: "<language>en-us</language>",
 		description: blogDescription,
-		items: (
-			await pagesGlobToRssItems(import.meta.glob("../content/blog/**/*.mdx"))
-		).map((inner) => ({
-			...inner,
-			link: inner.link
-				.replace(/^src\/content/, "")
-				.replace(/index.mdx$/, "")
-				.replaceAll(`'`, ""),
+		items: blogs.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			customData: post.data.customData,
+			link: `/blog/${post.slug}/`,
 		})),
 		site: context.site?.toString() ?? site,
 		title: "Goldblog",
